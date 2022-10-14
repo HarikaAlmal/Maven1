d5facfd38b50d519e2eb2c3884eaa56e7186de88
harik <harik@Harika>
2022-10-14 15:21:56 -0500

    try2

---
+++ MavenTestNGNew/src/test/java/com/Mavenpackage1/Mavenclass3.java
@@ -0,0 +1,17 @@
+package com.Mavenpackage1;
+
+import org.testng.annotations.Test;
+
+public class Mavenclass3 {
+
+	
+	@Test
+	public void test7() {
+		System.out.println("in test1");
+	}
+	
+	@Test
+	public void test8() {
+		System.out.println("in test1");
+	}
+}
